ce1c23555c7e4bdd787e47255231eb052554ae83
Lucas Cimaglia <lucascimaglia@gmail.com>
2025-08-01 00:05:32 -0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/CreaTuLanding1-Cimaglia/',
   plugins: [react()],
-  base: '/CreaTuLanding1-Cimaglia/'
-});
+})
+
 
