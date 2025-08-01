ddc76013f7fbd0178088378255b4ab4ba7abf13d
Lucas Cimaglia <lucascimaglia@gmail.com>
2025-07-31 23:40:11 -0300

    Deploy inicial

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/CreaTuLanding1-Cimaglia/' // nombre de tu repositorio
+});
+
